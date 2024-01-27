a804a452f2e0aad45284a07be20c1ef91d3bcb84
Suryaansh <surya94191@gmail.com>
2024-01-27 20:05:55 +0530

    set_pairs modify

--- STL/Set/set_pairs.cpp
+++ STL/Set/set_pairs.cpp
@@ -4,4 +4,18 @@ using namespace std;
 
 int main () {
 
+    set<pair<int, int>> sp;
+
+    sp.insert({4,10});
+    sp.insert({6,15});
+    sp.insert({23,30});
+    sp.insert({32,3});
+    sp.insert({46,60});
+
+    for(pair<int, int> x: sp) {
+
+        cout << "{" << x.first << x.second << "}, ";
+    }
+    
+    cout << endl;
 }
